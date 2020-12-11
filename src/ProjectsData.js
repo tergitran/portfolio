2980d1d6c57188c71b94ea0790a679a29809888e
tergitran <tergitran@gmail.com>
2020-12-11 14:15:48 +0700

    update projects

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -1,25 +1,36 @@
 const data = [
-    {
-        name: 'Shopping Cart',
-        description: 'Allows user to order product online. Select the number of items and views the summary order. Handles routing in React app with React Router',
-        jpeg_name: 'shopping-cart',
-        source_url: 'https://github.com/tergitran/shopping-cart',
-        web_url: 'https://tergitran.github.io/shopping-cart'
-    },
-    {
-        name: 'Weather App',
-        description: 'Get the forecast for today. You can search weather by city name. Data from AccuWeather APIs',
-        jpeg_name: 'weather-app',
-        source_url: 'https://github.com/tergitran/weather-app',
-        web_url: 'https://tergitran.github.io/weather-app'
-    },
-    {
-        name: 'Memory Card',
-        description: 'Get fun with Mini Memory game. You have to remember the cards you have selected',
-        jpeg_name: 'memory-card',
-        source_url: 'https://github.com/tergitran/memory-card',
-        web_url: 'https://tergitran.github.io/memory-card'
-    }
-]
+  {
+    name: "Shopping Cart",
+    description:
+      "Allows user to order product online. Select the number of items and views the summary order. Handles routing in React app with React Router",
+    jpeg_name: "shopping-cart",
+    source_url: "https://github.com/tergitran/shopping-cart",
+    web_url: "https://tergitran.github.io/shopping-cart",
+  },
+  {
+    name: "Weather App",
+    description:
+      "Get the forecast for today. You can search weather by city name. Data from AccuWeather APIs",
+    jpeg_name: "weather-app",
+    source_url: "https://github.com/tergitran/weather-app",
+    web_url: "https://tergitran.github.io/weather-app",
+  },
+  {
+    name: "Memory Card",
+    description:
+      "Get fun with Mini Memory game. You have to remember the cards you have selected",
+    jpeg_name: "memory-card",
+    source_url: "https://github.com/tergitran/memory-card",
+    web_url: "https://tergitran.github.io/memory-card",
+  },
+  {
+    name: "Movies Collection",
+    description:
+      "If you're interested in watching movies, don't ignore my website",
+    jpeg_name: "movies-collection",
+    source_url: "https://github.com/tergitran/movies-collection",
+    web_url: "https://tergitran.github.io/movies-collection",
+  },
+];
 
 export default data;
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import styles from './Project.module.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import styles from "./Project.module.css";
+import PropTypes from "prop-types";
 
-const Project = props => {
-    const { data } = { ...props };
-    const { name, description, jpeg_name, source_url, web_url } = { ...data };
+const Project = (props) => {
+  const { data } = { ...props };
+  const { name, description, jpeg_name, source_url, web_url } = { ...data };
 
-    return (
-        <div className={styles.Project}>
-            <img src={process.env.PUBLIC_URL + `/thumbnails/${jpeg_name}.JPG`}></img>
-            <div cla>
-                <h2>{name}</h2>
-                <p>{description}</p>
-                <div className={styles.Buttons}>
-                    <a href={source_url} target='_blank' className={styles.Link}>View Code</a>
-                    <a href={web_url} target='_blank' className={styles.Link}>Live Preview</a>
-                </div>
-            </div>
+  return (
+    <div className={styles.Project}>
+      <img src={process.env.PUBLIC_URL + `/thumbnails/${jpeg_name}.JPG`}></img>
+      <div>
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <div className={styles.Buttons}>
+          <a href={source_url} target="_blank" className={styles.Link}>
+            View Code
+          </a>
+          <a href={web_url} target="_blank" className={styles.Link}>
+            Live Preview
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-Project.propTypes = {
-
-};
+Project.propTypes = {};
 
 export default Project;
